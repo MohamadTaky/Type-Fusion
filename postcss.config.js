b729ef1f90de9fbb36861088c2a166e2abca077a
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-04-21 05:17:02 +0200

    modified tailwind darkmode classes

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ export default {
 	plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
-		...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+		...(import.meta.env.PROD ? { cssnano: {} } : {}),
 	},
 };

85054794b8b3ac8462dbcecdf5f7462d470f42f4
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-04-22 16:05:56 +0200

    fixed error boundary color

--- src/components/fallbacks/suspenseFallback.component.tsx
+++ src/components/fallbacks/suspenseFallback.component.tsx
@@ -3,7 +3,7 @@ import { CircleNotch } from "@phosphor-icons/react";
 export default function SuspenseFallback() {
 	return (
 		<div className="grid place-items-center place-self-center">
-			<CircleNotch size="96" weight="bold" className="animate-spin dark:text-gray-100" />
+			<CircleNotch size="96" weight="bold" className="animate-spin" />
 		</div>
 	);
 }
--- src/features/navigation/components/topbar.component.tsx
+++ src/features/navigation/components/topbar.component.tsx
@@ -15,37 +15,7 @@ export default function TopBar() {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="flex items-center justify-end gap-4 border-b border-gray-300 bg-gray-200 p-2 dark:border-hatai-600 dark:bg-hatai-800">
-			<div className="relative">
-				<button onClick={() => setIsOpen(prev => !prev)}>
-					<Translate size="35" className="box-content pt-1" />
-				</button>
-				<AnimatePresence>
-					{isOpen && (
-						<motion.ul
-							initial={{ height: 0, opacity: 0 }}
-							animate={{ height: "auto", opacity: 1 }}
-							exit={{ height: 0, opacity: 0 }}
-							transition={{ duration: 0.12 }}
-							className="absolute z-50 overflow-hidden rounded-md border border-gray-400 text-center ltr:right-0 rtl:left-0 dark:border-hatai-600">
-							<li
-								onClick={() => {
-									i18n.changeLanguage("en");
-									setIsOpen(false);
-								}}
-								className="cursor-pointer bg-gray-300 px-4 py-2 hover:bg-gray-400 dark:bg-hatai-700 dark:hover:bg-hatai-600">
-								English
-							</li>
-							<li onClick={() => {i18n.changeLanguage("ar"); setIsOpen(false)}}>
-								<p className="cursor-pointer bg-gray-300 px-4 py-2 hover:bg-gray-400 dark:bg-hatai-700 dark:hover:bg-hatai-600">
-									العربية
-								</p>
-							</li>
-						</motion.ul>
-					)}
-				</AnimatePresence>
-			</div>
-
+		<div className="flex items-center gap-4 border-b border-gray-300 bg-gray-200 p-2 dark:border-hatai-600 dark:bg-hatai-800">
 			<button onClick={toggleDarkMode}>
 				<AnimatePresence mode="wait">
 					{darkMode ? (
@@ -69,6 +39,39 @@ export default function TopBar() {
 					)}
 				</AnimatePresence>
 			</button>
+			<div className="relative">
+				<button onClick={() => setIsOpen(prev => !prev)}>
+					<Translate size="35" className="box-content pt-1" />
+				</button>
+				<AnimatePresence>
+					{isOpen && (
+						<motion.ul
+							initial={{ height: 0, opacity: 0 }}
+							animate={{ height: "auto", opacity: 1 }}
+							exit={{ height: 0, opacity: 0 }}
+							transition={{ duration: 0.12 }}
+							className="absolute z-50 overflow-hidden rounded-md border border-gray-400 text-center ltr:left-0 rtl:right-0 dark:border-hatai-600">
+							<li
+								onClick={() => {
+									i18n.changeLanguage("en");
+									setIsOpen(false);
+								}}
+								className="cursor-pointer bg-gray-300 px-4 py-2 hover:bg-gray-400 dark:bg-hatai-700 dark:hover:bg-hatai-600">
+								English
+							</li>
+							<li
+								onClick={() => {
+									i18n.changeLanguage("ar");
+									setIsOpen(false);
+								}}>
+								<p className="cursor-pointer bg-gray-300 px-4 py-2 hover:bg-gray-400 dark:bg-hatai-700 dark:hover:bg-hatai-600">
+									العربية
+								</p>
+							</li>
+						</motion.ul>
+					)}
+				</AnimatePresence>
+			</div>
 		</div>
 	);
 }

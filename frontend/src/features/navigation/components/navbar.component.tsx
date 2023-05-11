66f95214c58389c57f6fa0dbe0f9a295bc011080
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-05-11 16:15:49 +0200

    backend

--- frontend/src/features/navigation/components/navbar.component.tsx
+++ frontend/src/features/navigation/components/navbar.component.tsx
@@ -8,6 +8,7 @@ import useSignout from "~/features/auth/hooks/useSignout.hook";
 export default function Navbar() {
 	const { t } = useTranslation();
 	const { data: userAuth } = useUserAuthQuery();
+	console.log(userAuth);
 	const { mutate: signout } = useSignout();
 	return (
 		<aside className="row-span-2 border-r border-fill-2 bg-fill-3">
@@ -26,11 +27,11 @@ export default function Navbar() {
 					{userAuth ? (
 						<button
 							onClick={() => signout()}
-							className="flex items-center gap-2 text-start
-							bg-gray-300 bg-opacity-0 p-2 px-4 capitalize hover:bg-opacity-50
+							className="flex items-center gap-2 bg-gray-300
+							bg-opacity-0 p-2 px-4 text-start capitalize hover:bg-opacity-50
 							focus:bg-opacity-50 focus:outline-none dark:bg-hatai-700 dark:bg-opacity-0 dark:hover:bg-opacity-50 dark:focus:bg-opacity-50">
 							<SignOut weight="fill" size="25" />
-							<span className="whitespace-nowrap min-w-[11ch]">{t("sign out")}</span>
+							<span className="min-w-[11ch] whitespace-nowrap">{t("sign out")}</span>
 						</button>
 					) : (
 						<NavItem to="/auth" label="sign in" Icon={SignIn} />

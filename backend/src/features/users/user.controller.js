e276e411d505500c47ae98b26070f6237f7a14f5
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-05-11 13:16:43 +0200

    backend

--- backend/src/features/users/user.controller.js
+++ backend/src/features/users/user.controller.js
@@ -29,6 +29,7 @@ export async function singin(req, res, next) {
 			.cookie("token", token, {
 				httpOnly: true,
 				secure: true,
+				sameSite: "None",
 				expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 3),
 
 			})

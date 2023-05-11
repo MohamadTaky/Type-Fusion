7bfb71319f2cdd75e8219c41bf7eb23bb04352e1
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-05-12 00:41:13 +0200

    backend

--- frontend/src/libraries/axios/axiosInterceptor.ts
+++ frontend/src/libraries/axios/axiosInterceptor.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
-const client = axios.create({ baseURL: import.meta.env.VITE_API ?? "" });
+const client = axios.create({ baseURL: import.meta.env.VITE_API ?? "", withCredentials: true });
 
 export default function request({ ...options }: AxiosRequestConfig): Promise<AxiosResponse> {
 	return client(options) as Promise<AxiosResponse>;

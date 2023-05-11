be196c43ed37dece4ad8ab609fb15491af770d7d
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-05-11 23:53:40 +0200

    backend

--- frontend/src/libraries/axios/axiosInterceptor.ts
+++ frontend/src/libraries/axios/axiosInterceptor.ts
@@ -3,7 +3,5 @@ import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 const client = axios.create({ baseURL: import.meta.env.VITE_API ?? "" });
 
 export default function request({ ...options }: AxiosRequestConfig): Promise<AxiosResponse> {
-	const onError = (error: AxiosError) => error;
-	const onSuccess = (response: AxiosResponse) => response;
-	return client(options).then(onSuccess).catch(onError) as Promise<AxiosResponse>;
+	return client(options) as Promise<AxiosResponse>;
 }

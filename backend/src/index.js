e30c44d5dea0f2b0bae97aa0d26ea78459cf2f3a
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-05-11 23:12:56 +0200

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 
 config();
 const app = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: process.env.CLIENT_URL ?? "http://192.168.1.106:3000" }));

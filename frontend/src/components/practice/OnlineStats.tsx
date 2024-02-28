ac68c7248923c594c9fb36fc53aa9eddbc4cbe65
Mohamad Taky <mohamadtaky2002@gmail.com>
2024-02-28 23:16:09 +0200

    useTestsQuery bug fix

--- frontend/src/components/practice/OnlineStats.tsx
+++ frontend/src/components/practice/OnlineStats.tsx
@@ -1,10 +1,14 @@
-import useTestsQuery from "@/hooks/stats/useTestsQuery";
-import Stats from "./Stats";
+import request from "@/libraries/axios/axiosInterceptor";
 import usePracticeStore from "@/store/usePracticeStore";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+import Stats from "./Stats";
 
 export default function OnlineStats() {
-  const { data, refetch } = useTestsQuery();
+  const { data, refetch } = useQuery(["tests"], getTests, {
+    select: (data) => data.data,
+    suspense: true,
+  });
   const errorCount = usePracticeStore((store) => store.errorCount);
   const currentQuoteIndex = usePracticeStore((store) => store.currentQuoteIndex);
   useEffect(() => {
@@ -18,4 +22,8 @@ export default function OnlineStats() {
       errorCount={errorCount}
     />
   );
+}
+
+async function getTests() {
+  return request({ url: "/api/data", withCredentials: true });
 }
--- frontend/src/hooks/stats/useTestsQuery.ts
+++ frontend/src/hooks/stats/useTestsQuery.ts
@@ -1,16 +1,18 @@
 import request from "@/libraries/axios/axiosInterceptor";
 import { useQuery } from "@tanstack/react-query";
 import useUserAuthQuery from "../auth/useUserAuthQuery.hook";
+import useStatsPersistedStore from "./useStatsPersistedStore";
 
 export default function useTestsQuery() {
   const { data: userAuth } = useUserAuthQuery();
+  const store = useStatsPersistedStore();
   const query = useQuery(["tests"], getTests, {
     select: (data) => data.data,
     enabled: !!userAuth,
     suspense: true,
   });
 
-  return query;
+  return query.data ?? store;
 }
 
 async function getTests() {
